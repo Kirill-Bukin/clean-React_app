1fce15748db91a0d8c40db5a99e78a95e7215575
Kirill <kenny27041994@gmail.com>
2022-04-04 21:10:12 +0300

    init(react-app) add sass modules

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import { App } from "./components/App/App";
 
-ReactDOM.render(
-  <h1>Hello World!!!</h1>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById("root"));
